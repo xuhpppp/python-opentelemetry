b08a00de70ab84b688b0d4f90bd9768698f930f0
phucdt <phucdt@ominext.com>
2025-05-06 13:20:57 +0700

    create child spans

--- src/orders/services.py
+++ src/orders/services.py
@@ -1,11 +1,21 @@
 from orders.models import Order, OrderDB
 from fastapi import HTTPException
+from sqlalchemy import select
+from orders.telemetries import tracer
 
 
 def get_order_by_id(order_id: int, db):
-    order = db.query(OrderDB).filter(OrderDB.id == order_id).first()
-    if order is None:
-        raise HTTPException(status_code=404, detail="Order not found")
+    with tracer.start_as_current_span("service.get_order_by_id"):
+        # build the query
+        query = select(OrderDB).where(OrderDB.id == order_id)
+        with tracer.start_as_current_span("db.query_order_by_id") as span:
+            span.set_attribute("db.system", "postgresql")
+            span.set_attribute("db.statement", str(query))
 
-    # return Order.from_orm(order)
-    return Order.model_validate(order)
+            # execute the query
+            order = db.execute(query).scalar_one_or_none()
+        if order is None:
+            raise HTTPException(status_code=404, detail="Order not found")
+
+        # return Order.from_orm(order)
+        return Order.model_validate(order)
